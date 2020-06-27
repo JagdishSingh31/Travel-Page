f6c31d1774960c80f68bc9f24777f6a5d49f4227
Jagdish <singhjagdish2031@gmail.com>
2020-06-27 13:25:07 +0530

    script added

--- script.js
+++ script.js
@@ -1,26 +1,64 @@
-function initMap() { 
+/*--------NavMenu------------*/
 
-        var location = {lat: -25.363, lng: 131.044}; 
+document.querySelector(".fa-bars").addEventListener("click",menu);
 
-        var map = new google.maps.Map(document.getElementById('googleMap'), { 
+function menu(){
+    document.querySelector(".nav-bar").style.height="370px";
+    document.querySelector(".fa-bars").style.display="none";
+   document.querySelector(".fa-times").style.display="block";
+   
+}
 
-          zoom: 4, 
+document.querySelector(".fa-times").addEventListener("click",Close);
 
-          center:location 
-
-        });
-
-        var marker = new google.maps.Marker({ 
-
-          position: location, 
-
-          map: map 
-
-        }); 
-        
-        
-      } 
+function Close(){
+    document.querySelector(".nav-bar").style.height="70px";
+    document.querySelector(".fa-bars").style.display="block";
+   document.querySelector(".fa-times").style.display="none";
+   
+}
 
 
-<script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBMcmjpKGVbHoPdpqcbNSIKAeOoVqOubAk&callback=initMap">
-</script>
+/*------------MAP-------------*/
+
+//-------ImageSlider----------//
+var database={
+        "0":"image/Gallery1.jpg",
+        "1":"image/Gallery2.jpg",
+        "2":"image/Gallery3.jpg"
+};
+var img=document.createElement("img");
+img.src=database[0]
+document.querySelector(".gallery").appendChild(img);
+
+function slider(){
+   var index=0;
+   document.querySelector(".gallery").appendChild(img);
+   setInterval(function(){
+   
+document.getElementById(index).style.background="rgba(0,0,0,0.5)";
+    img.src=database[index]
+     document.querySelector(".gallery").appendChild(img);
+      
+         index++; 
+       if(index>=3){
+           slider();
+           index=0
+           var i,x
+          x=document.querySelectorAll(".dot");
+          for(i=0;i< x.length;i++){
+           x[i].style.background="rgba(255,255,255,0.5)";
+            
+              }
+          }
+      
+       
+   },5000)
+   var i,x
+          x=document.querySelectorAll(".dot");
+          for(i=0;i< x.length;i++){
+           x[i].style.background="rgba(255,255,255,0.5)";
+            
+          }
+}
+slider()
